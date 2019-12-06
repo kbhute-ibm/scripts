04e1f5e0ea571b064c563861df0a939d9e31c83d
kbhute-ibm <Koumudini.Bhute@ibm.com>
2019-12-06 17:24:12 +0530

    Update build_cfssl.sh

--- CFSSL/1.4.1/build_cfssl.sh
+++ CFSSL/1.4.1/build_cfssl.sh
@@ -3,7 +3,7 @@
 # LICENSE: Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 #
 # Instructions:
-# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/CFSSL/1.3.4/build_cfssl.sh
+# Download build script: wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/CFSSL/1.4.1/build_cfssl.sh
 # Execute build script: bash build_cfssl.sh    (provide -h for help)
 
 set -e -o pipefail
